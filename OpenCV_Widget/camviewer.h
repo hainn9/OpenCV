7fe47fe9530b0e7b98b909e880a901a6405a56ae
hainn9 <32747375+hainn9@users.noreply.github.com>
2017-10-26 23:05:07 +0700

    Capture video from camera

---
+++ OpenCV_Widget/camviewer.h
@@ -0,0 +1,21 @@
+#ifndef CAMVIEWER_H
+#define CAMVIEWER_H
+
+#include <QGraphicsView>
+#include <QImage>
+class CamViewer : public QGraphicsView
+{
+public:
+    CamViewer(QWidget *centralWidget) :QGraphicsView(centralWidget) {}
+    CamViewer(QGraphicsScene *scene, QWidget *parent = 0) : QGraphicsView(scene, parent) {}
+    ~CamViewer();
+    void setImage(QImage im);
+
+protected:
+    void paintEvent(QPaintEvent* paintEventInfo);
+
+private:
+    QImage image;
+};
+
+#endif // CAMVIEWER_H
---
+++ OpenCV_Widget/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "camviewer.h"
+#include <opencv2/highgui/highgui.hpp>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    bool isCameraOn;
+
+private slots:
+    void on_pushButton_clicked();
+    void updateView(void);
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene scene;
+    cv::VideoCapture video;
+    cv::Mat frame;
+};
+
+#endif // MAINWINDOW_H
